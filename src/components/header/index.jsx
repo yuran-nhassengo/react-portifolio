2710c8da104df32fc491bf4f4a7b34aabcf7ef30
yuran-nhassengo <yurannhassengo@gmail.com>
2024-07-23 02:01:34 +0200

    Organize my Header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { NavBar } from './navbar';
 
 export const Header =() =>{
   return (
-    <div>
-      
-    </div>
-  )
+    
+      <>
+        <header className="flex items-center justify-between bg-black text-white h-20 px-8 text-lg shadow-lg border-b border-yellow-DEFAULT">
+          
+          <div className="flex items-center">
+            <img 
+              src="/logo.jpeg" 
+              alt="Logo" 
+              className="h-16 w-auto border border-yellow-DEFAULT rounded-md"
+            />
+          </div>
+          <NavBar />
+        </header>
+      </>
+    );
+  
 }
--- src/components/header/navbar.jsx
+++ src/components/header/navbar.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
 export const NavBar =() =>{
+  
+  
+  const activeStyles = "text-yellow-DEFAULT font-bold";
+  const inactiveStyles = "text-gray-dark";
+
+
   return (
-    <div>
-      
-    </div>
+    <>
+    <nav className='space-x-4'>
+        <NavLink to ={'/'} className={({ isActive }) =>
+          `${isActive ? activeStyles : inactiveStyles}`}>Home</NavLink>
+
+        <NavLink to ={'/portifolio'} className={({ isActive }) =>
+          isActive ? activeStyles : inactiveStyles}>Projects</NavLink>
+
+        <NavLink to ={'/servicos'} className={({ isActive }) =>
+          isActive ? activeStyles : inactiveStyles}>About</NavLink>
+
+        <NavLink to ={'/contactos'} className={({ isActive }) =>
+          isActive ? activeStyles : inactiveStyles}>Resume</NavLink>
+
+    </nav>
+    </>
   )
 }
