0c491b002253c4238b0b2d0ca0b713de132e740f
yurannhassengo <yurannhassengo@gmail.com>
2024-08-18 01:16:02 -0700

    update header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -28,9 +28,6 @@ export const Header =() =>{
             </div>
 
             <div className="relative hidden md:flex items-center justify-center gap-3">
-              {/* <i className="bx bx-search absolute left-3 text-gray-500"></i>
-              <input type="text" placeholder="Search...."
-              className="py-2 pl-10 rounded-xl border-2 border-blue-300 focus:bg-slate-100 focus:outline-sky-500" /> */}
               <SearchComponent/>
             </div>
 
