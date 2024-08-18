4cdac864d2fd99a443acca2a1c75194eebc877f1
yurannhassengo <yurannhassengo@gmail.com>
2024-08-18 02:03:50 -0700

    add Home component

---
+++ src/components/about/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        <h1>Sobre mim</h1>
+        <p></p>
+      
+    </div>
+  )
+}
+
+
---
+++ src/components/home/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div>
+      <p>Sou Yuran Oscar Nhassengo, uma pessoa com um forte senso de responsabilidade, lealdade e justiça. 
+        Valorizo o trabalho em equipe e me considero um líder natural, sempre buscando colaborar e 
+        apoiar meus colegas. Comprometido com o crescimento contínuo e a busca por novos conhecimentos,
+         tenho uma paixão por enfrentar desafios e alcançar o sucesso coletivo.
+
+        No âmbito técnico, sou proficiente em Figma, HTML5, CSS, e tenho experiência com ReactJS, 
+        Node.js, Express.js, e TailwindCSS. Também sou familiarizado com ferramentas como Vite, 
+        e bancos de dados MySQL e MongoDB. Estou sempre pronto para contribuir e inovar em projetos,
+         mantendo uma atitude positiva e proativa.</p>
+    </div>
+  )
+}
+
+
