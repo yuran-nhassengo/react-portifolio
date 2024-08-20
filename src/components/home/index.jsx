846d36cccb79fe6aeda7fe33f64b2bb558370a9c
yurannhassengo <yurannhassengo@gmail.com>
2024-08-20 09:57:27 -0700

    update home

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -3,10 +3,14 @@ import { Element } from 'react-scroll'
 
 export const Home = () => {
   return (
-    <Element name="home" >
-        <div className=" max-w-md w-full mx-auto p-6 bg-white rounded-lg shadow-md mt-4 mb-4">
-
-        </div >
+    <Element name="home" className="flex" >
+        <div className="w-36 h-36 mx-auto p-0 bg-white rounded-full shadow-md mt-4 mb-4 flex items-center justify-center">
+            <img 
+                src="https://via.placeholder.com/150" 
+                alt="Imagem Circular" 
+                className="w-full h-full rounded-full object-cover"
+            />
+        </div>
         <div className=" max-w-md w-full mx-auto p-6 bg-white rounded-lg shadow-md mt-4 mb-4">
           <p >Ola, meu nome é Yuran Oscar Nhassengo, uma pessoa com um forte senso de responsabilidade, lealdade e justiça. 
             Valorizo o trabalho em equipe e me considero um líder natural, sempre buscando colaborar e 
