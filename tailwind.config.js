ab347b3434a543ac25e3160d4a38c5182d6088d0
yurannhassengo <yurannhassengo@gmail.com>
2024-08-24 12:23:39 -0700

    add extend content

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ export default {
   content: ["./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      content:{
+        about:'url(./about.svg)',
+      },
+    },
   },
   plugins: [],
 }
