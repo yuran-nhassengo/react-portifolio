e29e4e9af0cba0283005bd168c699dee09cd8e87
yurannhassengo <yurannhassengo@gmail.com>
2024-08-25 02:46:15 -0700

    add scroller

--- src/components/workExperience/cardExperience/index.jsx
+++ src/components/workExperience/cardExperience/index.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 
 export const ExperienceCard = ({details}) => {
   return (
-    <div className="md:m-0 border-solid border-2 rounded-lg p-6 my-0 mx-4">
-       <h1 className="md:m-0 text-2xl font-medium mb-3">{details.title}</h1>
+    <div className="md:m-0 border-solid border-2 bg-red-300 rounded-lg p-6 my-0 mx-4">
+       <h1 className="md:m-0 text-black text-2xl font-medium mb-3">{details.title} akskalalsl</h1>
 
-       <div className="inline-block text-xs font-normal rounded-md py-2 px-3 mb-6">
-            {details.responsibilities.map((item,index) =>(
-                <li className="md:text-xs list-none text-xs font-normal relative
-                after:content-[''] after:w-2 after:h-2 after:rounded-lg 
-                after:absolute after:-left-5 after:top-1" key={index}>{item}</li>
+       <div className="inline-block text-black text-xs font-normal rounded-md py-2 px-3 mb-6">
+            {details.responsabilities.map((item) =>(
+                <li className="md:text-xs  text-black  text-xs font-normal 
+                            " key={item.title}>{item}</li>
+                
+                            
             )
 
             )}
--- src/components/workExperience/index.jsx
+++ src/components/workExperience/index.jsx
@@ -1,53 +1,65 @@
-import { Carousel } from 'flowbite-react'
-import React from 'react'
+import React, { useRef } from 'react'
+import Slider from 'react-slick';
+import { WORK_EXPERIENCE } from '../../utils/data';
+import { ExperienceCard } from './cardExperience';
+
+
 
 export const WorkExperience = () => {
+
+    const sliderRef = useRef();
+
+    const settings = {
+      dots:false,
+      infinite:true,
+      speed:500,
+      slidesToShow:2,
+      slidesToScroll:1,
+      arrows:false,
+      responsive:[
+        {
+          breakpoint:769,
+          settings:{
+            slidesToShow:1,
+            slidesToScroll:1,
+          },
+        },
+      ],
+    };
+
+    const slideRight = () => {
+        sliderRef.current.slickNext();
+    }
+
+    const slideLeft = () => {
+        sliderRef.current.slickPrev();
+    }
+
   return (
-    <div >
-       <div className='px-4 lg:px-14 max-w-screen-2xl mx-auto min-h-screen h-screen'>
+   <section className="flex flex-wrap items-center justify-center bg-yellow-200 my-16 h-96 w-1/2 mx-16 relative">
+        <h5 className="text-2xl text-black font-normal mb-3">Work Experience</h5>
 
-       <Carousel className='w-full mx-auto'> 
-        <div className="my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse items-center justify-between
-        gap-1 ">
-
-          <div className="bg-green-400">
-            <img src="" alt="imagem" />
-          </div>
-
-          <div className='md:w-1/2'>
-                <h1 className='text-5xl font-semibold mb-4 text-black md:w-3/4 leading-snug
-                '>Primeiro Titulo </h1>
-                <p className='text-neutralGrey text-base mb-8'>Um pequeno paragrafo</p>
-
-                <button className='px-7 py-2 bg-red-300 text-white rounded hover:bg-red-500 transition-all duration-300 
-                hover:-translate-y-4'>Register</button>
-
-          </div>
+        <div className=" md:mb-8 md:text-xl w-12 h-12 flex items-center justify-center  
+        rounded-lg border-solid border-2 bg-blue-300
+        absolute top-1/2  cursor-pointer -right-8 " onClick={slideRight}>
+        <i className="bx bx-chevron-right text-4xl"></i>
         </div>
 
+        <div className="md:mb-8 md:text-xl  w-12 h-12 flex items-center justify-center 
+        rounded-lg border-solid border-2 bg-blue-300
+        absolute top-1/2  cursor-pointer -left-8"  onClick={slideLeft}>
+        <i className="bx bx-chevron-left text-4xl">
 
-        <div className="my-28 md:my-8 py-8 flex flex-col md:flex-row-reverse items-center justify-between
-        gap-8 bg-blue-700">
-
-          <div>
-            <img src="" alt="imagem" />
-          </div>
-
-
-
-          <div className='md:w-1/2'>
-                <h1 className='text-5xl font-semibold mb-4 text-black md:w-3/4 leading-snug
-                '>Primeiro Titulo</h1>
-                <p className='text-pink-600 text-base mb-8'>Um pequeno paragrafo</p>
-                <button className='px-7 py-2  text-white rounded hover:bg-black transition-all duration-300 
-                hover:-translate-y-4'>Register</button>
-          </div>
-
+        </i>
         </div>
-        
-      </Carousel>
-
-       </div>
-    </div>
+        <Slider ref={sliderRef} {...settings}>
+          {WORK_EXPERIENCE.map((item) =>{
+          
+            <ExperienceCard key={item.title} details={item}/>
+          })}
+        </Slider>
+   </section>
   )
 }
+
+
